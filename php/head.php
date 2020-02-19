9a2b0d2a68a8bb586c91f11974e5538574cc9a5e
bmit-dia <alessandro.dimaria@espas.ch>
2020-02-19 10:40:21 +0100

    Initial upload
    Initial upload of the index.php additionally uploading the Bootstrap CSS and JS files.
    Creating the head.php file, which contains the meta infos of the "main" pages.

---
+++ php/head.php
@@ -0,0 +1,10 @@
+<?php
+    $headinfo = '<meta charset="utf-8">
+                <meta name="description" content="Portfolioseite von Alessandro Di Maria">
+                <meta name="keywords" content="Portfolio, HTML, CSS, Alessandro Di Maria, JS, PHP, Bootstrap">
+                <meta name="author" content="Alessandro Di Maria">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Alessandro Di Maria</title>
+                <link rel="stylesheet" href="bootstrap.css">'
+                
+?>

02cdbdeeed46dda362e7f7b0fae44fc2338785bb
bmit-dia <alessandro.dimaria@espas.ch>
2020-02-21 09:37:49 +0100

    Adding Content
    Die Webseite war noch ziemlich leer, also wird sie mit Content befüllt. Das Design habe ich noch nicht überprüft, dies wird jedoch im nächsten Schritt getan.

--- index.php
+++ index.php
@@ -10,7 +10,33 @@
         ?>
     </head>
     <body>
-            <p>hello world</p>
-
+        <h1>Alessandro Di Maria</h1>
+        <h2>Über Mich</h2>
+        <h5>Privat</h5>
+        <p>
+            Mein Name lautet Alessandro Di Maria. Ich wurde am 9. Januar 1996 geboren und bin heute 24 Jahre alt.
+            Meine Hobbies belaufen sich auf das Spielen von Videospielen, Musik hören und mit Freunden etwas unternehmen.
+            Ich bin ein geselliger Typ und kann bei diversen Themen mitreden. Ich bilde mich gerne weiter, da ich der Ansicht bin,
+            dass ein grosses Wissen essentiel im Leben ist. Mein Traum ist es, etwas in der Welt zu hinterlassen, 
+            wofür man sich an mich erinnert. Ich lebe momentan in einer betreuten WG beim Kreuzplatz in Zürich. 
+        </p>
+        <h5>Beruflich</h5>
+        <p>
+            Momentan bin ich im 2. Lehrjahr meiner Ausbildung zum Informatiker mit Fachrichtung Applikationsentwicklung.
+            Meine Stärken liegen in den Websprachen HTML, CSS, JS, PHP, aber auch mit SQL-Datenbanken kenne ich mich aus.
+            Momentan lerne ich Springboot (Java) und hoffe, dort meine Webkenntnisse verbessern zu können. Ich kann mit 
+            Codeverwaltungssoftware wie Git umgehen und finde mich sowohl über die Bash, wie auch ein GUI zurecht.
+            Ein schönes Beispiel hierfür ist diese Website. Ich habe Visual Studio Code für die Entwicklung verwendet, 
+            mit Github Desktop auf ein Remote-Repository gepusht und ich lasse es von dort mittels eines Webhooks auf 
+            Metanet (Host der Seite) hochladen.<br/>
+            Ich habe mich dazu entschlossen die Seite mit Bootstrap aufzubauen, da heute immer seltener eigene Stylesheets
+            erstellt werden und ich mich dem Framework-Train anschliesse und mich mit Bootstrap befasse. Ich durfte für 
+            einen Auftrag auch schon Erfahrung mit dem CMS Wordpress machen und habe damit auch schon unsere abteilungsinterne
+            Bibliothek erweitern, indem ich den Passwortreseter und den Spamschutz implementierte.<br/>
+            Meine momentanen Projekte sind ein VBA-Scribt, welches die Überstunden des gesamten Firmenkaders aus deren Excelfiles
+            zusammenzählt und in eine Exceltabelle einträgt. Am zweiten Projekt arbeite ich schon ein wenig länger. Es handelt
+            sich um ein System, welches mittels E-Paper Displays die nächsten 3 Termine des jeweiligen Sitzungszimmers anzeigt.
+            Bei weiterem Interesse zu den Projekten, kann man über das Menü oder <a href="./pages/projekte.php">hier</a> klicken.
+        </p>
     </body>
 </html>
